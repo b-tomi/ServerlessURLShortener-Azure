be84da8bde834155b688bb904a8b7fc6dbc4b34b
Tomi <bdn.tomi@gmail.com>
2022-01-11 01:10:25 +0100

    Update link function

--- link/index.js
+++ link/index.js
@@ -2,9 +2,20 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const id = context.bindingData.id;
-    const responseMessage = `Link ID: ${id}`;
+    const homePage = "https://www.example.com/";
+    const errorPage = "https://www.example.com/";
+    const url = "https://www.example.com/";
+
+    // temp code just to make sure the id is extracted from request url
+    // const responseMessage = `Link ID: ${id}`;
+    // context.res = {
+    //     body: responseMessage
+    // };
 
     context.res = {
-        body: responseMessage
+        status: 302,
+        headers: {
+            "Location": url
+        }
     };
 }
