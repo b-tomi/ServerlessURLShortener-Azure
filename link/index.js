0bc4f54a6ae0b009a358419044851329a5a6c332
Tomi <bdn.tomi@gmail.com>
2022-01-13 09:09:38 +0100

    Update link function with logging logic

--- link/index.js
+++ link/index.js
@@ -3,7 +3,8 @@ module.exports = function (context, req) {
     
     const id = context.bindingData.id;
     const link = context.bindings.links;
-    const url = "https://www.example.com/";
+    const url = "https://www.tomi.sh/";
+    let response = "";
 
     // temp code just to make sure the id is extracted from request url
     // const responseMessage = `Link ID: ${id}`;
@@ -12,15 +13,16 @@ module.exports = function (context, req) {
     // };
 
     // temp code to help choose what parts to include in the logs
-    context.log("JavaScript queue trigger function processed work item");
-    context.log("========================context========================");
-    context.log(context);
-    context.log("========================req========================");
-    context.log(req);
+    // context.log("JavaScript queue trigger function processed work item");
+    // context.log("========================context========================");
+    // context.log(context);
+    // context.log("========================req========================");
+    // context.log(req);
 
     if (link) {
         if (link.active) {
             context.log("Link found & active: " + id);
+            response = "OK";
             context.res = {
                 status: 302,
                 headers: {
@@ -29,6 +31,7 @@ module.exports = function (context, req) {
             };
         } else {
             context.log("Link found but inactive: " + id);
+            response = "Inactive";
             context.res = {
                 status: 404,
                 body : "Link found but inactive",
@@ -39,6 +42,7 @@ module.exports = function (context, req) {
         }
     } else {
         context.log("Link not found: " + id);
+        response = "Invalid";
         context.res = {
             status: 404,
             body : "Link not found",
@@ -47,5 +51,17 @@ module.exports = function (context, req) {
             }
         };
     }
+
+    // write to table storage log
+    // making sure RowKey is unique
+    // including the complete context and req objects for now
+    context.bindings.logs = {
+        PartitionKey: id,
+        RowKey: context.bindingData.sys.utcNow + context.bindingData.sys.randGuid,
+        Context: JSON.stringify(context),
+        Request: JSON.stringify(req),
+        Response: response
+    };
+
     context.done();
 }
